920e7e42e99c004cd49b248d315f8b94c66f8007
Alan110 <306880673@qq.com>
2017-06-23 08:43:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <div class="navBar">
+      <div class="bar-left">
+        <div class="bar-action">我的图床</div>
+        <div class="bar-action">首页</div>
+        <div class="bar-action">我的上传</div>
+        <div class="bar-action">chrome插件</div>
+      </div>
+      <div class="bar-right">
+        <div class="bar-action">blog</div>
+        <div class="bar-action">注册</div>
+        <div class="bar-action">登录</div>
+        <div class="bar-action">设置</div>
+      </div>
+      <div></div>
+    </div>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style rel="stylesheet/css" lang="scss">
+body,html{
+   margin: 0;
+   padding: 0;
+   font-size: 14px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  .navBar{
+    background-color: #000;
+    color: #ccc;
+    height: 50px;
+    font-weight: bold;
+    line-height: 50px;
+  }
+
+  .bar-action{
+    text-align: center;
+    cursor: pointer;
+    margin: 0 20px;
+    &:hover{
+      color: #fff;
+    }
+  }
+
+  .navBar,.bar-left,.bar-right{
+    display: flex;
+  }
+
+  .bar-left{
+    width: 70%;
+  }
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="page-upload">
+      <div class="upload-wrapper">
+          <div class="upload">
+              <div class=""><img :src="icon_url" width="38px"/></div>
+              <div class="upload-tip">截图后粘贴、拖拽图片到这里上传</div>
+              <div class="upload-tip2">或者</div>
+              <div class="upload-btn-wrapper" style="">
+                <div class="upload-btn">点击选择文件</div>
+                <input class="upload-file"  type="file"/>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import upload_icon from '@/assets/upload_icon.png'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      icon_url :upload_icon
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.upload-wrapper{
+  width: 50%;
+  background-color: #f9f9f9;
+  height: 338px;
+  position: absolute;
+  left: 50%;
+  top : 30%;
+  transform: translate(-50%,-100px);
+}
+
+.upload{
+  text-align: center;
+  padding: 80px 0;
+}
+
+.upload-tip{
+  font-size: 28px;
+  color: #777777;
+}
+
+.upload-tip2{
+  color: #c3c3c3;
+  margin-top: 10px;
+}
+
+.upload-btn{
+  border-radius: 3px;
+  margin-top: 20px;
+  background-color: #5cb85c;
+  color: #fff;
+  width: 126px;
+  height: 38px;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 38px;
+
+  &:hover{
+    background-color: #449d44;
+  }
+}
+
+.upload-file{
+  cursor: pointer;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.upload-btn-wrapper{
+  position:relative;
+  display:flex;
+  justify-content : center;
+}
+
+
+</style>
